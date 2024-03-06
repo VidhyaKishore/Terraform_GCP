2011335d6a0bd2b464be82c2ba740c64814f289c
terraform <labguide@gmail.com>
2024-03-06 16:05:48 +0530

    This is my 1st commit

---
+++ main.tf
@@ -0,0 +1,21 @@
+provider "google" {
+  project     = "terraform-lab-414811"
+  region      = "us-central1"
+  zone        = "us-central1-a"
+}
+
+resource "google_compute_instance" "ubuntu_instance" {
+  name         = "ubuntu-instance1"
+  machine_type = "n1-standard-1"
+  zone         = "us-central1-a"
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-2004-lts"  # Ubuntu 20.04 LTS image
+    }
+  }
+
+  network_interface {
+    network = "default"  # Use the default network
+  }
+}
